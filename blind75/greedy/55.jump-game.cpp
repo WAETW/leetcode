d2784e5e34c72034a4439fefabfae1416adf009f
WAETW <zxc8110924@gmail.com>
2022-11-02 16:53:51 +0800

    restructure dir

--- blind75/greedy/55.jump-game.cpp
+++ blind75/greedy/55.jump-game.cpp
@@ -6,10 +6,16 @@
 
 // @lc code=start
 class Solution {
-public:
+   public:
     bool canJump(vector<int>& nums) {
-        
+        int n = nums.size();
+        int max_j = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i > max_j)
+                return false;
+            max_j = max(max_j, nums[i] + i);
+        }
+        return true;
     }
 };
 // @lc code=end
-
