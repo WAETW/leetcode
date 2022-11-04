9efa0a504c282b5245b1a453c41328f3b7b3b75c
WAETW <zxc8110924@gmail.com>
2022-11-04 18:13:20 +0800

    add 239 to sliding windows

---
+++ blind75/sliding_window/239.sliding-window-maximum.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=239 lang=cpp
+ *
+ * [239] Sliding Window Maximum
+ */
+
+// @lc code=start
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+   public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> ans;
+        int n = nums.size();
+        multiset<int> st;
+        for (int i = 0; i < k; ++i) {
+            st.insert(nums[i]);
+        }
+        ans.push_back(*st.rbegin());
+        for (int i = k; i < n; ++i) {
+            st.erase(st.find(nums[i - k]));
+            st.insert(nums[i]);
+            ans.push_back(*st.rbegin());
+        }
+        return ans;
+    }
+};
+// @lc code=end
